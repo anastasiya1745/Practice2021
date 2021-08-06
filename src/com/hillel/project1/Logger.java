810faa6f4894d1f96798fe521616197b7e4327d5
anastasiya1745 <anastasiya.od@gmail.com>
2021-08-06 20:07:55 +0300

    add class and update main

---
+++ src/com/hillel/project1/Logger.java
@@ -0,0 +1,23 @@
+package com.hillel.project1;
+
+public class Logger {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public void debug(String text){
+        System.out.println((ANSI_GREEN + "DEBUG: ") + ANSI_WHITE + text );
+    }
+    public void error(String text){
+        System.out.println((ANSI_RED + "ERROR: ") + ANSI_WHITE + text);
+    }
+    public void warn(String text){
+        System.out.println((ANSI_BLUE + "WARNING: ") + ANSI_WHITE + text);
+    }
+    public void info(String text){
+        System.out.println((ANSI_PURPLE + "INFO: ") + ANSI_WHITE + text);
+    }
+
+}
---
+++ src/com/hillel/project1/Main.java
@@ -0,0 +1,12 @@
+package com.hillel.project1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = new Logger();
+        logger.debug("debug message");
+        logger.warn("warning message");
+        logger.info("info message");
+        logger.error("error message");
+    }
+}
